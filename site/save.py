dfd34d9ba26d4f6448ef8df63ff2f82ab5b16365
askerry <amy.skerry@gmail.com>
2014-01-13 02:07:21 -0500

    first

---
+++ site/save.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import cgitb, cgi, MySQLdb, ast
+#import cgitb, cgi, ast
+import base64
+import random
+
+myform=cgi.FieldStorage()
+cgitb.enable()
+cursor = MySQLdb.connect(host="localhost",user="askerry",passwd="password",db="aesbehave").cursor()
+
+print 'Content-type:text/html\n\n'
+
+try:
+    rsvpname=myform['rsvpname']
+    rsvpval=myform['rsvpvalues']
+    contentvar="RSVP"
+    sql='insert into DLSRSVP (name,rsvp) values ("%s", "%s")' %(rsvpname, rsvpval)
+except:
+    pass
+try:
+    photo64=myform['photofield']
+    contentvar="photo"
+    sql='insert into DLSPHOTO (photo) values ("%s")' %(photo64)
+except:
+    pass
+try:
+    msg=myform['textmessage']
+    contentvar="text"
+    sql='insert into DLSMSG (msg) values ("%s")' %(msg)
+except:
+    pass
+try:
+    vid=myform['videosaved']
+    contentvar="video"
+except:
+    pass
+try:
+    cursor.execute(sql)
+except:
+    pass
+#g = open("/home/www/mindhive/saxe/aesvid/files/aesoutput"+str(random.randint(0,10000))+".jpg", "w")
+#g.write(base64.decodestring(photo64))
+#g.close()
+
+with open("saved.html", "r") as myfile: 
+    newhtml=myfile.read().replace('\n', '') 
+
+newhtml=newhtml.replace('CONTENTVARIABLESTRING',contentvar)
+print newhtml 
+
+
